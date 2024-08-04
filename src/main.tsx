e22865e79ab266c16421db398aa1308e2d28d6f0
Patrick Noah Policarpio <patrickpolicarpio08@gmail.com>
2024-08-04 20:55:26 +0800

    remove unused import

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { RouterProvider } from "react-router-dom";
